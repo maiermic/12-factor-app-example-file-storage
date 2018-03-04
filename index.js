92aba2a468e5dbfda3e46d266402b34e733348cb
Michael Maier <maier1michael@gmail.com>
2018-03-04 23:43:58 +0100

    upload single file

--- index.js
+++ index.js
@@ -7,23 +7,34 @@ const app = express();
 app.use(morgan('combined'));
 app.use(fileUpload());
 app.use('/', express.static(STATIC_IMAGE_DIRECTORY));
-app.post('/', async function (req, res) {
+app.put('/:filename', upload);
+app.post('/:filename', upload);
+
+async function upload(req, res) {
+    if (!req.params.filename) {
+        console.log('Missing filename in route');
+        return res.status(400).send('Missing filename in route')
+    }
     if (!req.files) {
+        console.log('No files were uploaded.');
         return res.status(400).send('No files were uploaded.');
     }
+    const files = Object.values(req.files);
+    if (files.length !== 1) {
+        console.log('Only one file can be uploaded.');
+        return res.status(400).send('Only one file can be uploaded.');
+    }
+    const file = files[0];
+    const filePath = path.join(STATIC_IMAGE_DIRECTORY, req.params.filename);
+    console.log(`save file to "${filePath}"`);
     try {
-        await Promise.all(
-            Object.values(req.files).map(file => {
-                const filePath = path.join(STATIC_IMAGE_DIRECTORY, file.name);
-                console.log(`save file to "${filePath}"`);
-                return file.mv(filePath);
-            })
-        );
+        await file.mv(filePath);
         res.send('Files uploaded!');
     } catch (err) {
-        return res.status(500).send(`Error saving files ${err}`);
+        res.status(500).send(`Error saving files ${err}`);
     }
-});
+}
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}!`);
 });
